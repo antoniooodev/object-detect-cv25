d9b70321bdbf3cf0d03d1c21d548c59f860476aa
cocopops9 <giuseppedauriaud@gmail.com>
2025-04-25 00:57:58 +0200

    Remove CMake build artifacts and binary from repo

--- src/CMakeFiles/object-detect.dir/dataloader.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/object-detect.dir/dataloader.cpp.o: \
- /home/giuseppe/Scrivania/lab_cv/object-detect-cv25/src/dataloader.cpp \
- /usr/include/stdc-predef.h /usr/include/c++/11/iostream
--- src/CMakeFiles/object-detect.dir/detection.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/object-detect.dir/detection.cpp.o: \
- /home/giuseppe/Scrivania/lab_cv/object-detect-cv25/src/detection.cpp \
- /usr/include/stdc-predef.h
